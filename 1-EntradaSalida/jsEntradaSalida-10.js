b3033669eacb6dcb15999ba22ec10b2106349d1b
perria <ailenperri.ap@gmail.com>
2016-07-03 17:04:18 -0300

    js 10 descuento 25%

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,13 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarAumento()
 {
+	var importe;
+	var descuento;
+	var resultado;
+
+    descuento=parseInt(importe)*0.25;
+	resultado=parseInt(importe)-parseInt(descuento);
 	
+	document.getElementById('resultado').value=resultado;
+	alert(resultado);
 }
