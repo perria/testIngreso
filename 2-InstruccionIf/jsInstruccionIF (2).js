1058fe83f97cdc0dc0ed5938f1278167ba5f791f
perria <ailenperri.ap@gmail.com>
2016-07-06 20:03:28 -0300

    Hasta el ejercicio 3, sin else porque no lo vimos todavia.

--- 2-InstruccionIf/jsInstruccionIF (2).js
+++ 2-InstruccionIf/jsInstruccionIF (2).js
@@ -1,7 +1,16 @@
 function Mostrar()
 {
 //tomo la edad  
+var edad;
+edad=document.getElementById('edad').value;
+if(edad>=18)
+{
+	alert("Usted es mayor de 18 años.");
+}
 
-
+else{
+	alert("Usted es menor de edad.");
+}
+//no hace falta el else porque solo pedia mayor.
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,6 +1,19 @@
 function Mostrar()
 {
 //tomo la edad  
+var edad;
+edad=document.getElementById('edad').value;
+if(edad>=18)
+{
+	alert("Usted es mayor de 18 años.");
+}
 
+if(edad<18)
+{
+	alert("Usted es menor de edad.")
+}
+
+//else{
+	//alert("Usted es menor de edad.");} Lo comento porque no se podía usar else.
 
 }//FIN DE LA FUNCIÓN
