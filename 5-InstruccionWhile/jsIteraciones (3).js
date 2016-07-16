55e3dc755d01b79e36cf97ed9abb7d262a965644
perria <ailenperri.ap@gmail.com>
2016-07-15 22:05:29 -0300

    fin

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -1,7 +1,13 @@
 function Mostrar()
 {
 
-var clave = prompt("ingrese el número clave.");
+var clave=prompt("ingrese el número clave.");
 
+while(clave!=utn750) 
+{	
+	prompt("ingrese el número clave.");
+}
+
+ alert("bravo");
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (5).js
+++ 5-InstruccionWhile/jsIteraciones (5).js
@@ -1,10 +1,18 @@
 function Mostrar()
 {
 
-var sexo = prompt("ingrese f ó m .");
+var sexo=prompt("ingrese f ó m .");
+
+while(sexo!=f || sexo!=m)
+{
+	sexo=prompt("ingrese f ó m .");
+}
 
 
 
 document.getElementById('Sexo').value=sexo;
 
 }//FIN DE LA FUNCIÓN
+
+
+//primero el 12, despues 5, 7, 9, 11. Si se logran hacer esos, se pasa a los otros.
